1eb5696efb679424be408a5f972fe14b9e6394c6
SajanThapa22 <sajanthapa1888@gmail.com>
2023-07-24 08:40:22 +0545

    Show a spinner while show a genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,28 @@
-import useData from "../hooks/useData";
-import useGenres, { Genre } from "../hooks/useGenres";
+import { List, ListItem, Image, HStack, Text, Spinner } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import croppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner size="xl" />;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={croppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            ></Image>
+            <Text fontSize="lg" key={genre.id}>
+              {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
